23efaa9ee61688f30566f3e7f714843de04b5d4e
hoangknguyen1807 <hoangknguyen.1807@gmail.com>
2019-11-12 00:12:07 +0700

    Add project files.

---
+++ OpenGL-App1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace OpenGL_App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vertexControl1 = new SharpGL.Controls.VertexControl();
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.colorPalette = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vertexControl1
+            // 
+            this.vertexControl1.Location = new System.Drawing.Point(540, 0);
+            this.vertexControl1.Name = "vertexControl1";
+            this.vertexControl1.Size = new System.Drawing.Size(8, 8);
+            this.vertexControl1.TabIndex = 0;
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.ForeColor = System.Drawing.SystemColors.Window;
+            this.openGLControl.Location = new System.Drawing.Point(13, 89);
+            this.openGLControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(738, 487);
+            this.openGLControl.TabIndex = 1;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            this.openGLControl.Load += new System.EventHandler(this.openGLControl_Load);
+            this.openGLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseDown);
+            this.openGLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseUp);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(13, 12);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(90, 54);
+            this.btnLine.TabIndex = 2;
+            this.btnLine.Text = "Line";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(117, 12);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(90, 54);
+            this.btnCircle.TabIndex = 3;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // colorPalette
+            // 
+            this.colorPalette.Location = new System.Drawing.Point(661, 12);
+            this.colorPalette.Name = "colorPalette";
+            this.colorPalette.Size = new System.Drawing.Size(90, 54);
+            this.colorPalette.TabIndex = 4;
+            this.colorPalette.Text = "Color Palette";
+            this.colorPalette.UseVisualStyleBackColor = true;
+            this.colorPalette.Click += new System.EventHandler(this.colorPalette_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 598);
+            this.Controls.Add(this.colorPalette);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.openGLControl);
+            this.Controls.Add(this.vertexControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.Controls.VertexControl vertexControl1;
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button colorPalette;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ OpenGL-App1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenGL_App1
+{
+    public partial class Form1 : Form
+    {
+        Color userColor;
+        short shape; // 0 - Line, 1 - Circle
+        Point pStart, pEnd;
+        public Form1()
+        {
+            InitializeComponent();
+            userColor = Color.White;
+            shape = 0;
+        }
+
+        private void openGLControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+        }
+
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+            // Create a perspective transformation.
+            gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height);
+            gl.Ortho2D(0, openGLControl.Width, 0, openGLControl.Height);
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.Color(userColor.R / 255.0, userColor.G / 255.0, userColor.B / 255.0);
+
+            // Vẽ vời chỗ này. Ví dụ:
+            if (shape == 0)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+                gl.End();
+                gl.Flush();
+            }
+            else
+            {
+                gl.Begin(OpenGL.GL_)
+
+            }
+
+
+
+            ////gl.Color(1f, 0, 0, 0); // Chọn màu đỏ
+            //gl.Begin(OpenGL.GL_TRIANGLES); // Chọn chế độ vẽ tam giác
+            //gl.Vertex2sv(new short[] { 0, 0 }); // Đỉnh thứ 1 tọa độ 0,0
+            //gl.Vertex2sv(new short[] { 100, 100 }); // Đỉnh thứ 2 tọa độ 100, 100
+            //gl.Vertex2sv(new short[] { 200, 0 }); // Đỉnh thứ 3 tọa độ 200, 0
+            //gl.End();
+            //gl.Flush();// Thực hiện lệnh vẽ ngay lập tức thay vì đợi sau 1 khoảng thời gian
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            shape = 0;
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            shape = 1;
+        }
+
+        private void colorPalette_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                userColor = colorDialog1.Color;
+            }
+        }
+
+        private void openGLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            pEnd = e.Location;
+        }
+
+        private void openGLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            pStart = e.Location;
+            pEnd = pStart;
+        }
+    }
+}
