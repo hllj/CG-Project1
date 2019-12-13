0374c53222f15409c768052c721b8e3ace0283ca
hoangknguyen1807 <hoangknguyen.1807@gmail.com>
2019-12-13 23:31:39 +0700

    Some UI layout fixes

--- OpenGL-App1/Form1.Designer.cs
+++ OpenGL-App1/Form1.Designer.cs
@@ -65,12 +65,12 @@
             this.openGLControl.DrawFPS = false;
             this.openGLControl.ForeColor = System.Drawing.SystemColors.Window;
             this.openGLControl.Location = new System.Drawing.Point(13, 140);
-            this.openGLControl.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.openGLControl.Margin = new System.Windows.Forms.Padding(5);
             this.openGLControl.Name = "openGLControl";
             this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
             this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
             this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
-            this.openGLControl.Size = new System.Drawing.Size(860, 436);
+            this.openGLControl.Size = new System.Drawing.Size(860, 556);
             this.openGLControl.TabIndex = 1;
             this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
             this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
@@ -86,7 +86,7 @@
             this.btnLine.Location = new System.Drawing.Point(13, 12);
             this.btnLine.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLine.Name = "btnLine";
-            this.btnLine.Size = new System.Drawing.Size(91, 54);
+            this.btnLine.Size = new System.Drawing.Size(79, 54);
             this.btnLine.TabIndex = 2;
             this.btnLine.Text = "Line";
             this.btnLine.UseVisualStyleBackColor = true;
@@ -94,10 +94,10 @@
             // 
             // btnCircle
             // 
-            this.btnCircle.Location = new System.Drawing.Point(117, 12);
+            this.btnCircle.Location = new System.Drawing.Point(98, 12);
             this.btnCircle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCircle.Name = "btnCircle";
-            this.btnCircle.Size = new System.Drawing.Size(91, 54);
+            this.btnCircle.Size = new System.Drawing.Size(65, 54);
             this.btnCircle.TabIndex = 3;
             this.btnCircle.Text = "Circle";
             this.btnCircle.UseVisualStyleBackColor = true;
@@ -105,10 +105,10 @@
             // 
             // colorPalette
             // 
-            this.colorPalette.Location = new System.Drawing.Point(948, 15);
+            this.colorPalette.Location = new System.Drawing.Point(1009, 13);
             this.colorPalette.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.colorPalette.Name = "colorPalette";
-            this.colorPalette.Size = new System.Drawing.Size(91, 54);
+            this.colorPalette.Size = new System.Drawing.Size(91, 53);
             this.colorPalette.TabIndex = 4;
             this.colorPalette.Text = "Color Palette";
             this.colorPalette.UseVisualStyleBackColor = true;
@@ -117,38 +117,39 @@
             // labelMode
             // 
             this.labelMode.AutoSize = true;
-            this.labelMode.Location = new System.Drawing.Point(816, 15);
+            this.labelMode.Location = new System.Drawing.Point(887, 667);
             this.labelMode.Name = "labelMode";
             this.labelMode.Size = new System.Drawing.Size(55, 17);
             this.labelMode.TabIndex = 6;
             this.labelMode.Text = "Mode : ";
+
             // 
             // btn_rectangle
             // 
-            this.btn_rectangle.Location = new System.Drawing.Point(213, 14);
+            this.btn_rectangle.Location = new System.Drawing.Point(267, 12);
             this.btn_rectangle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_rectangle.Name = "btn_rectangle";
-            this.btn_rectangle.Size = new System.Drawing.Size(107, 54);
+            this.btn_rectangle.Size = new System.Drawing.Size(89, 54);
             this.btn_rectangle.TabIndex = 3;
-            this.btn_rectangle.Text = "RECTANGLE";
+            this.btn_rectangle.Text = "Rectangle";
             this.btn_rectangle.UseVisualStyleBackColor = true;
             this.btn_rectangle.Click += new System.EventHandler(this.btnRectangle_Click);
             // 
             // btn_Triangles
             // 
-            this.btn_Triangles.Location = new System.Drawing.Point(325, 12);
+            this.btn_Triangles.Location = new System.Drawing.Point(171, 12);
             this.btn_Triangles.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_Triangles.Name = "btn_Triangles";
-            this.btn_Triangles.Size = new System.Drawing.Size(107, 54);
+            this.btn_Triangles.Size = new System.Drawing.Size(89, 54);
             this.btn_Triangles.TabIndex = 3;
-            this.btn_Triangles.Text = "TRIANGLES";
+            this.btn_Triangles.Text = "Triangle";
             this.btn_Triangles.UseVisualStyleBackColor = true;
             this.btn_Triangles.Click += new System.EventHandler(this.btn_Triangles_click);
             // 
             // btn_equipentagon
             // 
-            this.btn_equipentagon.Location = new System.Drawing.Point(439, 12);
-            this.btn_equipentagon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_equipentagon.Location = new System.Drawing.Point(577, 12);
+            this.btn_equipentagon.Margin = new System.Windows.Forms.Padding(4);
             this.btn_equipentagon.Name = "btn_equipentagon";
             this.btn_equipentagon.Size = new System.Drawing.Size(109, 54);
             this.btn_equipentagon.TabIndex = 7;
@@ -158,10 +159,10 @@
             // 
             // btn_EquiHexagon
             // 
-            this.btn_EquiHexagon.Location = new System.Drawing.Point(556, 12);
-            this.btn_EquiHexagon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_EquiHexagon.Location = new System.Drawing.Point(460, 12);
+            this.btn_EquiHexagon.Margin = new System.Windows.Forms.Padding(4);
             this.btn_EquiHexagon.Name = "btn_EquiHexagon";
-            this.btn_EquiHexagon.Size = new System.Drawing.Size(124, 54);
+            this.btn_EquiHexagon.Size = new System.Drawing.Size(109, 54);
             this.btn_EquiHexagon.TabIndex = 8;
             this.btn_EquiHexagon.Text = "EquiHexagon";
             this.btn_EquiHexagon.UseVisualStyleBackColor = true;
@@ -169,10 +170,10 @@
             // 
             // btn_Ellipse
             // 
-            this.btn_Ellipse.Location = new System.Drawing.Point(689, 14);
-            this.btn_Ellipse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Ellipse.Location = new System.Drawing.Point(363, 12);
+            this.btn_Ellipse.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Ellipse.Name = "btn_Ellipse";
-            this.btn_Ellipse.Size = new System.Drawing.Size(120, 53);
+            this.btn_Ellipse.Size = new System.Drawing.Size(89, 54);
             this.btn_Ellipse.TabIndex = 9;
             this.btn_Ellipse.Text = "Ellipse";
             this.btn_Ellipse.UseVisualStyleBackColor = true;
@@ -180,10 +181,10 @@
             // 
             // btn_Select
             // 
-            this.btn_Select.Location = new System.Drawing.Point(13, 74);
-            this.btn_Select.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Select.Location = new System.Drawing.Point(13, 72);
+            this.btn_Select.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Select.Name = "btn_Select";
-            this.btn_Select.Size = new System.Drawing.Size(91, 59);
+            this.btn_Select.Size = new System.Drawing.Size(79, 54);
             this.btn_Select.TabIndex = 10;
             this.btn_Select.Text = "Select";
             this.btn_Select.UseVisualStyleBackColor = true;
@@ -193,20 +194,20 @@
             // 
             this.btn_ColorFilling.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_ColorFilling.BackgroundImage")));
             this.btn_ColorFilling.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_ColorFilling.Location = new System.Drawing.Point(112, 74);
-            this.btn_ColorFilling.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_ColorFilling.Location = new System.Drawing.Point(98, 72);
+            this.btn_ColorFilling.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ColorFilling.Name = "btn_ColorFilling";
-            this.btn_ColorFilling.Size = new System.Drawing.Size(81, 59);
+            this.btn_ColorFilling.Size = new System.Drawing.Size(65, 54);
             this.btn_ColorFilling.TabIndex = 11;
             this.btn_ColorFilling.UseVisualStyleBackColor = true;
             this.btn_ColorFilling.Click += new System.EventHandler(this.btn_ColorFilling_Click);
             // 
             // btn_Translate
             // 
-            this.btn_Translate.Location = new System.Drawing.Point(231, 87);
-            this.btn_Translate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Translate.Location = new System.Drawing.Point(171, 72);
+            this.btn_Translate.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Translate.Name = "btn_Translate";
-            this.btn_Translate.Size = new System.Drawing.Size(100, 28);
+            this.btn_Translate.Size = new System.Drawing.Size(89, 54);
             this.btn_Translate.TabIndex = 12;
             this.btn_Translate.Text = "Translate";
             this.btn_Translate.UseVisualStyleBackColor = true;
@@ -214,10 +215,10 @@
             // 
             // btn_Rotate
             // 
-            this.btn_Rotate.Location = new System.Drawing.Point(363, 87);
-            this.btn_Rotate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Rotate.Location = new System.Drawing.Point(267, 72);
+            this.btn_Rotate.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Rotate.Name = "btn_Rotate";
-            this.btn_Rotate.Size = new System.Drawing.Size(100, 28);
+            this.btn_Rotate.Size = new System.Drawing.Size(89, 54);
             this.btn_Rotate.TabIndex = 13;
             this.btn_Rotate.Text = "Rotate";
             this.btn_Rotate.UseVisualStyleBackColor = true;
@@ -226,10 +227,10 @@
             // 
             this.groupBox1.Controls.Add(this.rbtn_Bound);
             this.groupBox1.Controls.Add(this.rbtn_ScanLine);
-            this.groupBox1.Location = new System.Drawing.Point(881, 140);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(890, 140);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(159, 84);
             this.groupBox1.TabIndex = 14;
             this.groupBox1.TabStop = false;
@@ -239,7 +240,7 @@
             // 
             this.rbtn_Bound.AutoSize = true;
             this.rbtn_Bound.Location = new System.Drawing.Point(9, 54);
-            this.rbtn_Bound.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtn_Bound.Margin = new System.Windows.Forms.Padding(4);
             this.rbtn_Bound.Name = "rbtn_Bound";
             this.rbtn_Bound.Size = new System.Drawing.Size(111, 21);
             this.rbtn_Bound.TabIndex = 1;
@@ -251,7 +252,7 @@
             this.rbtn_ScanLine.AutoSize = true;
             this.rbtn_ScanLine.Checked = true;
             this.rbtn_ScanLine.Location = new System.Drawing.Point(9, 25);
-            this.rbtn_ScanLine.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtn_ScanLine.Margin = new System.Windows.Forms.Padding(4);
             this.rbtn_ScanLine.Name = "rbtn_ScanLine";
             this.rbtn_ScanLine.Size = new System.Drawing.Size(92, 21);
             this.rbtn_ScanLine.TabIndex = 0;
@@ -263,7 +264,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1329, 598);
+            this.ClientSize = new System.Drawing.Size(1112, 703);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_Rotate);
             this.Controls.Add(this.btn_Translate);
